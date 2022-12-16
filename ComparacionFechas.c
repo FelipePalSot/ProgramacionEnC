c0532a2c518abf9826f8d4504934293fc35c0c62
Felipe Jean Franco Palomino Sotelo <87220639+FelipePalSot@users.noreply.github.com>
2022-12-15 22:22:26 -0500

    Add files via upload

--- ComparacionFechas.c
+++ ComparacionFechas.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
+long unir(int,int,int);
+
 int main()
 {
-	int d1, m1, a1, d2, m2, a2;
-	print("Ingresa un dia, mes, anio: ");
-	scanf("%d%d%d",&d1,&m1,&a1);
-	print("Ingresa otro dia, mes, anio: ");
-	scanf("%d%d%d",&d2,&m2,&a2);
+	int d1, m1, a1;
+	int d2, m2, a2;
+	long f1, f2;
 	
-	printf("La fecha es: %d/%d/%d\n",d1,m1,a1);
-	printf("La fecha es: %d/%d/%d\n",d2,m2,a2);
+	printf("Ingresa la fecha1: ");
+	scanf("%d %d %d",&d1,&m1,&a1);
+	
+	printf("Ingresa la fecha2: ");
+	scanf("%d %d %d",&d2,&m2,&a2);
+	
+	f1=unir(d1,m1,a1);
+	f2=unir(d2,m2,a2);
+	
+	if(f1>f2)
+	{
+		printf("La primera fecha es mayor\n");
+	}
+	if(f1<f2)
+	{
+		printf("La segunda fecha es mayor\n");
+	}
 	
 	return 0;
+}
+
+long unir(int d,int m,int a)
+{
+	int fecha= d+m*100+a*10000;
+	return fecha;
 }
